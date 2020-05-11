a9302e01887ca7f3b8972fdb31d86eb6fb975d96
rrd <rrd@1108.cc>
2020-05-11 16:19:56 +0200

    reordering code

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faClock, faMale, faChild, faUser, faAt, faPhone, faMoneyBill, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+Vue.config.productionTip = false
+
 library.add(faClock, faMale, faChild, faUser, faAt, faPhone, faMoneyBill, faInfoCircle)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.config.productionTip = false
 
 Vue.filter('toNumFormat', function(
   value,

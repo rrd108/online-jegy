817503f77855a872051b081e69acfa60c4f94863
rrd <rrd@1108.cc>
2021-06-05 13:26:09 +0200

    logo added

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,6 @@
 <template>
   <footer>
+    <img src="../assets/logo.svg" alt="">
   </footer>
 </template>
 
@@ -9,12 +10,18 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 footer {
   position: fixed;
   bottom: 0;
   background-color: #fff;
   width: 100%;
   height: 4rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+img {
+  height: 2.5rem;
 }
 </style>
